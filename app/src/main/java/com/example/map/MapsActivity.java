0fdb7ac6d6a632e6f2d1955955c0159568940337
mohamadH80 <mh.ostadi.v@gmail.com>
2021-09-29 21:55:13 +0330

    show user location and goSetting

--- app/src/main/java/com/example/map/MapsActivity.java
+++ app/src/main/java/com/example/map/MapsActivity.java
@@ -1,8 +1,23 @@
 package com.example.map;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -13,6 +28,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.map.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,6 +51,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //nh
 
 
+//        LocationManager lm=(LocationManager) getSystemService(LOCATION_SERVICE);
+//        lm.requestLocationUpdates(lm.GPS_PROVIDER,30000,0,);
+
+
     }
 
     /**
@@ -47,16 +69,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        Toast.makeText(getApplicationContext(), "choose start", Toast.LENGTH_SHORT).show();
+        mMap=googleMap;
 
         LatLng tehran = new LatLng(35.69, 51.39); //tehran
-//        mMap.addMarker(new MarkerOptions().position(tehran).title("marker is in tehran"));
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(tehran, 13);
         mMap.moveCamera(cameraUpdate);
 
+        enableLocation();
+    }
+
+    private void enableSearchButton() {
+        EditText searchField = (EditText) findViewById(R.id.sf);
+        Button searchButton = (Button) findViewById(R.id.sb);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String searchText = searchField.getText().toString();
+                Geocoder gc = new Geocoder(MapsActivity.this);
+                try {
+                    List<android.location.Address> list = gc.getFromLocationName(searchText, 2);
+                    Address a = list.get(0);
+                    LatLng latLng = new LatLng(a.getLatitude(), a.getLongitude());
+                    CameraUpdate ca = CameraUpdateFactory.newLatLng(latLng);
+                    mMap.moveCamera(ca);
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 
+    private void enableLocation(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            /* && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED*/) {
+
+        }
+        else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+        directSetting();
+        mMap.setMyLocationEnabled(true);
+
+    }
+
+    private void directSetting()
+    {
+        Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+    }
 }
